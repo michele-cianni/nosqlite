f158a7311371d39bf8052dfd0474766bb7261b7d
Michele Cianni <cianni.michele.private@gmail.com>
2024-12-06 18:48:56 +0100

    Create CommandParsingException.java

---
+++ core/src/main/java/com/github/michele/cianni/nosqlite/core/command/parser/CommandParsingException.java
@@ -0,0 +1,7 @@
+package com.github.michele.cianni.nosqlite.core.command.parser;
+
+public class CommandParsingException extends Exception {
+    public CommandParsingException(String message) {
+        super(message);
+    }
+}
