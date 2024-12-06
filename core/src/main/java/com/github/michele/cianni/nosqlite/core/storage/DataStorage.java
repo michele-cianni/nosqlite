317277803fbcc86e48fd332290531f0c4b707527
Michele Cianni <cianni.michele.private@gmail.com>
2024-12-06 18:45:23 +0100

    Update DataStorage.java

--- core/src/main/java/com/github/michele/cianni/nosqlite/core/storage/DataStorage.java
+++ core/src/main/java/com/github/michele/cianni/nosqlite/core/storage/DataStorage.java
@@ -2,15 +2,16 @@ package com.github.michele.cianni.nosqlite.core.storage;
 
 import com.github.michele.cianni.nosqlite.core.models.Entry;
 
+import java.io.IOException;
 import java.util.Collection;
 
 public interface DataStorage {
 
-    void save(String key, Entry entry);
+    void save(String key, Entry entry) throws IOException;
+
+    void delete(String key) throws IOException;
 
     Entry load(String key);
-
-    void delete(String key);
 
     Collection<Entry> loadAll();
 }
