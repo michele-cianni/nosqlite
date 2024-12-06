a7bfe602ee6995e3fda2824791b09ce272ce9afa
Michele Cianni <cianni.michele.private@gmail.com>
2024-12-06 18:46:37 +0100

    Create ErrorMessages.java

---
+++ core/src/main/java/com/github/michele/cianni/nosqlite/core/utils/ErrorMessages.java
@@ -0,0 +1,9 @@
+package com.github.michele.cianni.nosqlite.core.utils;
+
+public final class ErrorMessages {
+        public static final String EMPTY_COMMAND = "Empty command";
+        public static final String INVALID_PARAMETERS = "Invalid parameters: %s";
+        public static final String DATABASE_MUST_NOT_BE_NULL = "Database must not be null";
+        public static final String KEY_MUST_NOT_BE_NULL = "Key must not be null";
+        public static final String ENTRY_MUST_NOT_BE_NULL = "Entry must not be null";
+}
