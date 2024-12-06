54e994bd8a430837e7f37b34aaefc51432927d16
Michele Cianni <cianni.michele.private@gmail.com>
2024-12-06 18:49:03 +0100

    Create CommandResult.java

---
+++ core/src/main/java/com/github/michele/cianni/nosqlite/core/command/CommandResult.java
@@ -0,0 +1,47 @@
+package com.github.michele.cianni.nosqlite.core.command;
+
+import java.util.Arrays;
+
+public record CommandResult(boolean status, String message, Object... values) {
+
+    /**
+     * Create a successful command result.
+     *
+     * @param message the message
+     * @param values the values
+     * @return a successful command result
+     */
+    public static CommandResult success(String message, Object... values) {
+        return new CommandResult(true, message, values);
+    }
+
+    /**
+     * Create a failed command result.
+     *
+     * @param message the message
+     * @param values the values
+     * @return a failed command result
+     */
+    public static CommandResult failure(String message, Object... values) {
+        return new CommandResult(false, message, values);
+    }
+
+    /**
+     * Create an error command result.
+     *
+     * @param message the message
+     * @return an error command result
+     */
+    public static CommandResult error(String message) {
+        return new CommandResult(false, message);
+    }
+
+    @Override
+    public String toString() {
+        return "CommandResult{" +
+                "status=" + status +
+                ", message='" + message + '\'' +
+                ", values=" + Arrays.toString(values) +
+                '}';
+    }
+}
